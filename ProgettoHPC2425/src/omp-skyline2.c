5e959cdf0d14373abea9e045727725dbd2f76c6f
Luca Venturi <luca.venturi29@studio.unibo.it>
2025-02-26 19:02:11 +0100

    Create git repo for High-Perfomance-Computing Project

---
+++ ProgettoHPC2425/src/omp-skyline2.c
@@ -0,0 +1,239 @@
+/****************************************************************************
+ *
+ * skyline.c - Serial implementaiton of the skyline operator
+ *
+ * Copyright (C) 2024 Moreno Marzolla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------------
+ *
+ * Questo programma calcola lo skyline di un insieme di punti in D
+ * dimensioni letti da standard input. Per una descrizione completa
+ * si veda la specifica del progetto sulla piattaforma "Virtuale".
+ *
+ * Per compilare:
+ *
+ *      gcc -std=c99 -Wall -Wpedantic -O2 skyline.c -o skyline
+ *
+ * Per eseguire il programma:
+ *
+ *      ./skyline < input > output
+ *
+ ****************************************************************************/
+
+#if _XOPEN_SOURCE < 600
+#define _XOPEN_SOURCE 600
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "hpc.h"
+
+typedef struct {
+    float *P;   /* coordinates P[i][j] of point i               */
+    int N;      /* Number of points (rows of matrix P)          */
+    int D;      /* Number of dimensions (columns of matrix P)   */
+} points_t;
+
+/**
+ * Read input from stdin. Input format is:
+ *
+ * d [other ignored stuff]
+ * N
+ * p0,0 p0,1 ... p0,d-1
+ * p1,0 p1,1 ... p1,d-1
+ * ...
+ * pn-1,0 pn-1,1 ... pn-1,d-1
+ *
+ */
+void read_input( points_t *points )
+{
+    char buf[1024];
+    int N, D;
+    float *P;
+
+    if (1 != scanf("%d", &D)) {
+        fprintf(stderr, "FATAL: can not read the dimension\n");
+        exit(EXIT_FAILURE);
+    }
+    assert(D >= 2);
+    if (NULL == fgets(buf, sizeof(buf), stdin)) { /* ignore rest of the line */
+        fprintf(stderr, "FATAL: can not read the first line\n");
+        exit(EXIT_FAILURE);
+    }
+    if (1 != scanf("%d", &N)) {
+        fprintf(stderr, "FATAL: can not read the number of points\n");
+        exit(EXIT_FAILURE);
+    }
+    P = (float*)malloc( D * N * sizeof(*P) );
+    assert(P);
+    for (int i=0; i<N; i++) {
+        for (int k=0; k<D; k++) {
+            if (1 != scanf("%f", &(P[i*D + k]))) {
+                fprintf(stderr, "FATAL: failed to get coordinate %d of point %d\n", k, i);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    points->P = P;
+    points->N = N;
+    points->D = D;
+}
+
+void free_points( points_t* points )
+{
+    free(points->P);
+    points->P = NULL;
+    points->N = points->D = -1;
+}
+
+/* Returns 1 iff |p| dominates |q| */
+int dominates( const float * p, const float * q, int D )
+{
+    /* The following loops could be merged, but the keep them separated
+       for the sake of readability */
+    for (int k=0; k<D; k++) {
+        if (p[k] < q[k]) {
+            return 0;
+        }
+    }
+    for (int k=0; k<D; k++) {
+        if (p[k] > q[k]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * Compute the skyline of `points`. At the end, `s[i] == 1` iff point
+ * `i` belongs to the skyline. The function returns the number `r` of
+ * points that belongs to the skyline. The caller is responsible for
+ * allocating the array `s` of length at least `points->N`.
+ */
+int skyline( const points_t *points, int *s )
+{
+    const int D = points->D;
+    const int N = points->N;
+    const float *P = points->P;
+    int r = 0;
+
+    int local_s[N];
+
+    #pragma omp parallel for
+    for (int i=0; i<N; i++) {
+        s[i] = 1;
+        local_s[i] = 1;
+    }
+
+    // NON VA BENE, ogni thread non è aggiornato con i valori di s, dovrebbe esserci una sincronizzazione ad ogni iterazione e non solo alla fine.
+    // Non essendo sincronizzate r conta più volte lo stesso punto, i punti nell'insieme finale sono però corretti.
+    #pragma omp parallel for schedule(dynamic) reduction(-:r) reduction(&&:local_s[:N])
+    for (int i=0; i<N; i++) {
+        if ( local_s[i] ) {
+            for (int j=0; j<N; j++) {
+                if ( local_s[j] && dominates( &(P[i*D]), &(P[j*D]), D ) ) {
+                    local_s[j] = 0;
+                }
+            }
+        }
+    }
+
+    #pragma omp parallel for reduction(+:r)
+    for (int i = 0; i < N; i++) {
+        s[i] = local_s[i];
+        r += (s[i] ? 1 : 0);
+    }
+
+    return r;
+}
+
+/**
+ * Print the coordinates of points belonging to the skyline `s` to
+ * standard ouptut. `s[i] == 1` iff point `i` belongs to the skyline.
+ * The output format is the same as the input format, so that this
+ * program can process its own output.
+ */
+void print_skyline( const points_t* points, const int *s, int r )
+{
+    const int D = points->D;
+    const int N = points->N;
+    const float *P = points->P;
+
+    printf("%d\n", D);
+    printf("%d\n", r);
+    for (int i=0; i<N; i++) {
+        if ( s[i] ) {
+            for (int k=0; k<D; k++) {
+                printf("%f ", P[i*D + k]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main( int argc, char* argv[] )
+{
+    points_t points;
+
+    if (argc != 1) {
+        fprintf(stderr, "Usage: %s < input_file > output_file\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    read_input(&points);
+    int *s = (int*)malloc(points.N * sizeof(*s));
+    assert(s);
+
+#ifdef TESTING
+    int r;
+    double elapsed[10];
+    for (size_t i = 0; i < 10; i++)
+    {
+        fprintf(stderr, "Iteration %ld\n", i);
+        const double tstart = hpc_gettime();
+        r = skyline(&points, s);
+        elapsed[i] = hpc_gettime() - tstart;
+        fprintf(stderr, "Completed in (s) %f\n", elapsed[i]);
+        fprintf(stderr, "r: %d\n", r);
+    }
+
+    const double avg = (elapsed[0] + elapsed[1] + elapsed[2] + elapsed[3] + elapsed[4] + elapsed[5] + elapsed[6] + elapsed[7] + elapsed[8] + elapsed[9]) / 10;
+
+#else
+    const double tstart = hpc_gettime();
+    const int r = skyline(&points, s);
+    const double elapsed = hpc_gettime() - tstart;
+
+#endif
+
+    print_skyline(&points, s, r);
+
+    fprintf(stderr, "\n\t%d points\n", points.N);
+    fprintf(stderr, "\t%d dimensions\n", points.D);
+    fprintf(stderr, "\t%d points in skyline\n\n", r);
+
+#ifdef TESTING
+    fprintf(stderr, "Average execution time:  %f (s)\n", avg);
+#else
+    fprintf(stderr, "Execution time (s) %f\n", elapsed);
+#endif
+
+    free_points(&points);
+    free(s);
+    return EXIT_SUCCESS;
+}
